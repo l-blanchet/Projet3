708a10b41d2ee3918e49f174ebcca559e2a1d2ae
Louis <louisblanchet33@gmail.com>
2018-10-08 14:58:15 +0200

    fin du premier mode et rajout de correctios de bug (avec le zero en tete de liste qui n'etait pas pris en compte )

--- src/com/louisBlanchet/PlusOuMoinsMode1.java
+++ src/com/louisBlanchet/PlusOuMoinsMode1.java
@@ -1,5 +1,9 @@
 package com.louisBlanchet;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -14,9 +18,13 @@ public class PlusOuMoinsMode1 extends Randomizer {
     public void mode1(){
         Randomizer test = new Randomizer();
         String nbMystereDecoupe[] = test.getRandomized();
+        String nbMysteredecoupe1[]= nbMystereDecoupe;
         int nbEssais = 7;
         System.out.println("Vous avez sélectionné le mode 1, vous avez "+nbEssais+" essais, bonne chance !");
         for (int compteur =0 ; compteur<nbEssais;compteur++) {
+            System.out.println(Arrays.toString(nbMystereDecoupe));
+            int exlegeek = 1;
+            exlegeek = exlegeek +1 ;
             System.out.println("proposition:");
             Scanner sc = new Scanner(System.in);
             int essai = 0;
@@ -38,7 +46,73 @@ public class PlusOuMoinsMode1 extends Randomizer {
                 String decoupage = proposition.substring(compteurtest, compteurtest + 1);
                 propositionDecoupe[compteurtest] = decoupage;
             }
-            System.out.println(nbMystereDecoupe);
+
+            String [] reponse = new String[proposition.length()];
+
+            for (int verificateur = 0 ; verificateur<proposition.length(); verificateur++){
+                String nbMystere;
+                nbMystere = nbMystereDecoupe[verificateur] ;
+                int test1 = Integer.parseInt(nbMystere);
+                String proposition1 ;
+                proposition1 = propositionDecoupe[verificateur];
+                int test2 = Integer.parseInt(proposition1);
+
+                if (test2==test1){
+                    reponse[verificateur] = "=";
+                }
+                else if (test2<test1){
+                    reponse[verificateur] = "-";
+                }
+                 else if (test2>test1 ){
+                    reponse[verificateur] = "+";
+                }
+
+            }
+            System.out.println("réponse" + Arrays.toString(reponse));
+
+            if (Arrays.equals(propositionDecoupe, nbMysteredecoupe1) || compteur == 6){
+                if (Arrays.equals(propositionDecoupe, nbMysteredecoupe1)) {
+                    System.out.println("Vous avez gagné en " + exlegeek + " essais, Bravo!");
+                }
+                if (compteur == 6){
+                    System.out.println("Vous avez perdu");
+                }
+                System.out.println("veuillez sélectionner dans quelle section voulez vous aller");
+                System.out.println("1- Rejouer");
+                System.out.println("2- Menu Principal");
+                System.out.println("3- Quitter");
+                 sc = new Scanner(System.in);
+                int selection = 0;
+                try {
+                    selection = sc.nextInt();
+                } catch (InputMismatchException e) {
+                    System.out.println("veillez à ne rentrer que des chiffres !");
+                }
+                if (selection == 1){
+                    mode1();
+                }
+                else if (selection == 2){
+                    MainMenu main = new MainMenu();
+                    main.displayMenu();
+                }
+                else if (selection == 3){
+                    System.out.println("fermeture du programme ");
+                    System.exit(0);
+                }
+                else{
+                     sc = new Scanner(System.in);
+                    selection = 0;
+                    try {
+                        selection = sc.nextInt();
+                    } catch (InputMismatchException e) {
+                        System.out.println("veillez à ne rentrer que des chiffres !");
+                    }
+                }
+            }
+
+
+            System.out.println(Arrays.toString(nbMystereDecoupe));
+            System.out.println(Arrays.toString(propositionDecoupe));
         }
     }
 }
