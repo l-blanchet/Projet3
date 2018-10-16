c8aa432f80e5408d071dc6f328bb02d67fd9f97b
Louis <louisblanchet33@gmail.com>
2018-10-16 11:04:11 +0200

    changement dans le menu 3

--- src/com/louisBlanchet/Menu3.java
+++ src/com/louisBlanchet/Menu3.java
@@ -4,38 +4,16 @@ import java.util.Scanner;
 
 public class Menu3 {
     public void specialMenu3 (){
-        System.out.println("Vous êtes à présent dans les options " );
-        System.out.println("1- Options Avancées");
-        System.out.println("2- explications des modes de jeu ");
-        System.out.println("3- retour ");
+        System.out.println("Voici les explications des différents modes " );
+        System.out.println("Mode Challenger : Vous devez deviner le code/combinaison de l'ordinateur ");
+        System.out.println("Mode Défenseur : Vous devez protéger votre code/combinaison à l'ordinateur ");
+        System.out.println("Mode Hybride : Vous devez à la fois proteger votre code/combinaison à l'ordinateur et deviner le code/combinaison de l'ordinateur ");
+        System.out.println("Appuyez sur 1 pour revenir à la page précédente");
 
         Scanner sc = new Scanner(System.in);
         int nbMenu3 = sc.nextInt();
 
         if (nbMenu3 == 1){
-            System.out.println("cette section n'est pas encore disponible veuillez revenir plus tard");
-            specialMenu3();
-        }
-        if (nbMenu3 == 2){
-            System.out.println("Mode Challenger : Vous devez deviner le code/combinaison de l'ordinateur ");
-            System.out.println("Mode Défenseur : Vous devez protéger votre code/combinaison à l'ordinateur ");
-            System.out.println("Mode Hybride : Vous devez à la fois proteger votre code/combinaison à l'ordinateur et deviner le code/combinaison de l'ordinateur ");
-            System.out.println("Appuyez sur 1 pour revenir à la page précédente");
-            int nbMenuspe;
-            do {
-            Scanner sc1 = new Scanner(System.in);
-             nbMenuspe = sc1.nextInt();
-
-                if (nbMenuspe == 1) {
-                    specialMenu3();
-                }
-                if (nbMenuspe != 1) {
-                    System.out.println("veuillez entrer le bon chiffre ");
-
-                }
-            }while (nbMenuspe != 1);
-        }
-        if (nbMenu3 == 3){
             MainMenu main = new MainMenu();
             main.displayMenu();
         }
