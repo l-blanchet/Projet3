909d2628c879d12dd724a01a31f888f38f57423a
Louis <louisblanchet33@gmail.com>
2018-11-13 11:52:00 +0100

    intégration des conditions de victoire et de défaite et correction de qulques problèmes d'affichages

--- src/com/louisBlanchet/PlusOuMoinsMode2.java
+++ src/com/louisBlanchet/PlusOuMoinsMode2.java
@@ -6,21 +6,21 @@ import java.util.Scanner;
 
 public class PlusOuMoinsMode2 {
 
-    private String[] nbPropose = {"5","5","5","5"};
-    private String[] nbProposePrecedent;
-    private String[] reponse = {"=","=","=","="};
-    private int lenght = 4;
-    private int compteur =0;
     String signe1;
-    private int boucleAlgo =4;
     int chiffreCourantTest;
+    int chiffre;
+    private String[] nbPropose = {"5", "5", "5", "5"};
+    private String[] nbProposePrecedent;
+    private String[] reponse = {"=", "=", "=", "="};
+    private String[] nbMystereDecoupe = {"1", "1", "1", "1"};
+    private int lenght = 4;
+    private int compteur = 0;
+    private int boucleAlgo = 4;
+
 
     public PlusOuMoinsMode2(int lenght) {
         this.lenght = lenght;
     }
-
-
-
 
     public String[] recupNbMystère() {
         int lenght = 4;
@@ -37,42 +37,51 @@ public class PlusOuMoinsMode2 {
             }
         } while (nbMystere < 1000);
         String nbMystere1 = Integer.toString(nbMystere);
-        String[] nbMystereDecoupe = new String[nbMystere1.length()];
 
         for (int i = 0; i < nbMystere1.length(); i++) {
             String decoupage = nbMystere1.substring(i, i + 1);
             nbMystereDecoupe[i] = decoupage;
         }
-        System.out.println(Arrays.toString(nbMystereDecoupe));
         this.mode2();
         return nbMystereDecoupe;
 
     }
-    private void mode2(){
+
+    private void mode2() {
         do {
-            //todo afficher la valeur par défaut [5,5,5,5]
             if (compteur == 0) {
-                compteur = compteur+1;
+
                 this.affichePropositionParDefaut();
+                compteur = compteur + 1;
+                // System.out.println(Arrays.toString(nbPropose));
                 this.saisieUserReponse();
             }
-            else {
-                this.nbPropose();
-            }
-                //todo faire saisir à l'utilisateur les "+" et les "-"
-                this.saisieUserReponse();
-                //todo verification de victoire /defaite /continuer
-                //todo si continuer relancer un tour si besoin
-                //todo si victoire/défaite afficher le menu de fin de jeu
+            this.nbPropose();
+            compteur = compteur + 1;
 
-        }while (compteur<5);
+            this.saisieUserReponse();
+            //todo verification de victoire /defaite /continuer
+            this.verification();
+            //todo si continuer relancer un tour si besoin
+            //todo si victoire/défaite afficher le menu de fin de jeu
+
+        } while (compteur <= 5);
 
 
+    }
+
+    private void affichePropositionParDefaut() {
+
+        System.out.println(Arrays.toString(nbPropose));
     }
 
     private void saisieUserReponse() {
         boolean testrecup = true;
         String reponseNonDecoupe = null;
+        System.out.println(Arrays.toString(reponse));
+        System.out.println(Arrays.toString(nbPropose));
+        System.out.println(Arrays.toString(nbMystereDecoupe));
+        System.out.println(compteur);
         do {
             System.out.println("Veuillez rentrer des + et des - en vous basant sur la reponse de l ordinateur par rapport à la votre ");
             Scanner sc = new Scanner(System.in);
@@ -90,60 +99,78 @@ public class PlusOuMoinsMode2 {
             reponse[i] = decoupage;
         }
         chiffreCourantTest = 0;
-        System.out.println(Arrays.toString(reponse));
-        System.out.println(Arrays.toString(nbPropose));
-        System.out.println("test réussi");
-        System.out.println(compteur);
 
-        this.mode2();
+
+        this.verification();
     }
 
-    private void affichePropositionParDefaut() {
+    private void verification() {
+        if (Arrays.equals(nbPropose, nbMystereDecoupe)) {
+            System.out.println("vous avez perdu");
 
-        System.out.println(Arrays.toString(nbPropose));
+        } else if (compteur >= 5) {
+            System.out.println("Vous avez gagné !");
+        } else {
+            this.mode2();
+        }
+        int selection;
+        do {
+            System.out.println("veuillez sélectionner dans quelle section voulez vous aller");
+            System.out.println("1- Rejouer");
+            System.out.println("2- Menu Principal");
+            System.out.println("3- Quitter");
+
+            Scanner sc = new Scanner(System.in);
+            selection = 0;
+            try {
+                selection = sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("veillez à ne rentrer que des chiffres !");
+            }
+            if (selection == 1) {
+                recupNbMystère();
+            } else if (selection == 2) {
+                MainMenu main = new MainMenu();
+                main.displayMenu();
+            } else if (selection == 3) {
+                System.out.println("fermeture du programme ");
+                System.exit(0);
+            }
+
+        } while ((selection < 1) || (selection > 3));
     }
-    int chiffre;
 
     public String[] nbPropose() {
         nbProposePrecedent = nbPropose;
-        int test = lenght-1 ;
+        int test = lenght - 1;
         int boucletest = 0;
 
 
+        for (int boucle = (test); boucle >= 0; boucle--) {
+            int chiffreprec = Integer.parseInt(String.valueOf(nbProposePrecedent[boucletest]));
 
-            for (int boucle = (test); boucle >= 0; boucle--) {
-                int chiffreprec = Integer.parseInt(String.valueOf(nbProposePrecedent[boucletest]));
-
-                signe1 = String.valueOf(reponse[boucletest]);
-                boucletest = boucletest +1;
-                chiffre = proposechiffre(0, chiffreprec, signe1);
-                System.out.println(chiffre);
+            signe1 = String.valueOf(reponse[boucletest]);
+            boucletest = boucletest + 1;
+            chiffre = proposechiffre(0, chiffreprec, signe1);
 
 
-            }
+        }
         String chiffreString = Integer.toString(chiffre);
         for (int i = 0; i < chiffreString.length(); i++) {
             String decoupage = chiffreString.substring(i, i + 1);
             nbPropose[i] = decoupage;
         }
         boucleAlgo = 4;
-        return nbPropose ;
+        return nbPropose;
 
 
     }
-
-
-
 
 
     public int proposechiffre(int chiffreCourant, int chiffrePrecedent, String signe) {
         int valeurMax = 10;
         int valeurMin = 1;
         int resultat = 0;
-
-
-
-
 
 
         if (chiffrePrecedent == 3 && signe1.equals("+")) {
@@ -153,7 +180,7 @@ public class PlusOuMoinsMode2 {
         if (chiffrePrecedent == 7 && signe1.equals("-")) {
             valeurMin = 5;
         }
-        if (signe1.equals("=")){
+        if (signe1.equals("=")) {
             resultat = chiffrePrecedent;
             boucleAlgo--;
         }
@@ -170,7 +197,7 @@ public class PlusOuMoinsMode2 {
             boucleAlgo--;
         }
 
-        int chiffreCourantPartiel = (int) (resultat*(Math.pow (10, boucleAlgo)));
+        int chiffreCourantPartiel = (int) (resultat * (Math.pow(10, boucleAlgo)));
         chiffreCourantTest = chiffreCourantPartiel + chiffreCourantTest;
         chiffreCourant = chiffreCourantTest;
         return chiffreCourant;
